35dc9c18dc7da3f1312cfc3a0ca39039a97ca8ee
HaseebElectronics <126021521+HaseebElectronics@users.noreply.github.com>
2023-02-21 22:44:49 +0200

    Add files via upload

---
+++ mot_cont.ino
@@ -0,0 +1,29 @@
+void setup() {
+  pinMode (3, INPUT);
+  pinMode (4, OUTPUT);
+  pinMode (5, OUTPUT);
+  pinMode (6, OUTPUT);
+  pinMode (7, OUTPUT);
+  digitalWrite(4, LOW);
+  digitalWrite(5, LOW);
+  digitalWrite(6, LOW);
+  digitalWrite(7, LOW);
+}
+
+void loop() {
+  byte inputPulse=digitalRead(3);
+  if (inputPulse == HIGH){
+    digitalWrite (4,HIGH);
+    digitalWrite (5,HIGH);
+    delay (8000);
+    digitalWrite (5,LOW);
+    digitalWrite (6,HIGH);
+    delay (8000);
+    digitalWrite (6,LOW);
+    digitalWrite (7,HIGH);
+    delay (8000);
+    digitalWrite (7,LOW);
+    digitalWrite (4,LOW);
+    setup();
+    }
+}
